e13b7e99055d24113e166c0e2883ca874d75cffb
Your Name <you@example.com>
2025-12-02 13:10:14 +0000

    Test auto-deploy workflow

--- projecthub-backend/src/server.js
+++ projecthub-backend/src/server.js
@@ -8,6 +8,7 @@ mongoose.connect(process.env.MONGO_URI)
     console.log('Mongo connected');
     app.listen(PORT,'0.0.0.0', () => {
       console.log('Server is running on port : ' + PORT);
+      console.log("Auto-deploy test!");
     });
   })
   .catch(err => console.error(err));
