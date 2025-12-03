c3ba552d8e3a6c74e2baf5cbabfe98475e3de12d
Faeiz <faeizhamdard@gmail.com>
2025-12-03 15:24:05 +0000

    CI test

---
+++ server.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+import app from './app.js';
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Mongo connected');
+    app.listen(PORT,'0.0.0.0', () => {
+      console.log('Server is running on port : ' + PORT);
+      console.log("Auto-deploy test... ");
+      console.log("CI test");
+    });
+  })
+  .catch(err => console.error(err));
