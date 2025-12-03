470ebe1305e160b78d556c3c7455e5362cc4fb63
Faeiz <faeizhamdard@gmail.com>
2025-12-03 16:02:26 +0000

    CI test3

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ mongoose.connect(process.env.MONGO_URI)
       console.log('Server is running on port : ' + PORT);
       console.log("Auto-deploy test... ");
       console.log("CI test 2");
+      console.log("CI test 3");
     });
   })
   .catch(err => console.error(err));
